8875c14faa24abf1026197d49bcc390b792d63eb
woodpeckershop <chris.sun5567@gmail.com>
2021-11-09 22:40:18 -0800

    add resolve function

--- schema/scheme.js
+++ schema/scheme.js
@@ -1,6 +1,11 @@
 const graphql = require("graphql");
+const { resolve } = require("path/posix");
+const_ = require("lodash");
 
-const { GraphQlObjectType, GraphQlString } = graphql;
+const { GraphQlObjectType, GraphQlString, GraphQlSchema } = graphql;
+
+//dummy data
+var books = [{ name: "sss", genre: "fantasy", id: "1" }];
 
 const BookType = new GraphQlObjectType({
   name: "Book",
@@ -9,4 +14,22 @@ const BookType = new GraphQlObjectType({
     name: { type: GraphQlString },
     genre: { type: GraphQlString },
   }),
+});
+
+const RootQuery = new GraphQlObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQlString } },
+      resolve(parent, args) {
+        //code to get data from db
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQlSchema({
+  query: RootQuery,
 });
